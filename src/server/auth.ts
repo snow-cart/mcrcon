0a193e5179d3936ecb3eb8b33e00a844b6fef52e
Mykolas Spu <mykolasspu@gmail.com>
2024-06-10 14:19:02 +0300

    add facebook OAuth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,6 +8,7 @@ import { type Adapter } from "next-auth/adapters";
 
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -57,6 +58,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: env.FACEBOOK_CLIENT_ID,
+      clientSecret: env.FACEBOOK_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
